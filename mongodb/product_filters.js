7f832613a95eb36fafae7097e1618cf5fb6e57f4
sanausengeny-glitch <sanausengeny@gmail.com>
2026-07-24 16:22:39 +0300

    mongodb

---
+++ mongodb/product_filters.js
@@ -0,0 +1,13 @@
+# MongoDB Results
+
+Query Results
+
+Wireless Mouse - $25
+
+Bluetooth Headphones - $120
+
+Aggregation Results
+
+Electronics - Average Rating 4.67
+Home Appliances - Average Rating 4.67
+Furniture - Average Rating 4.33
